9f117f380183133f762c4bf738568504fbff8eba
1Zholdoshbek <zholdoshbekyashiev@gmail.com>
2025-09-12 13:33:49 +0600

    create payment service app

---
+++ payment-service-app/src/main/java/com/example/paymentserviceapp/model/Payment.java
@@ -0,0 +1,40 @@
+package com.example.paymentserviceapp.model;
+
+public class Payment {
+
+    private long id;
+    private double value;
+
+    public Payment() {
+
+    }
+    public Payment(long id, double value) {
+        this.id = id;
+        this.value = value;
+    }
+
+    public long getId() {
+        return id;
+    }
+
+    public void setId(long id) {
+        this.id = id;
+    }
+
+    public double getValue() {
+        return value;
+    }
+
+    public void setValue(double value) {
+        this.value = value;
+    }
+
+    @Override
+    public String toString() {
+        return "Payment{" +
+                "id=" + id +
+                ", value=" + value +
+                '}';
+    }
+}
+
