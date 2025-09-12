0a97b9a8b48452a6cc0ae4edb82a51da03233b37
1Zholdoshbek <zholdoshbekyashiev@gmail.com>
2025-09-12 13:33:49 +0600

    create payment service app

---
+++ payment-service-app/src/main/java/com/example/paymentserviceapp/controller/PaymentController.java
@@ -0,0 +1,36 @@
+package com.example.paymentserviceapp.controller;
+import com.example.paymentserviceapp.model.Payment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.util.ArrayList;
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+@RestController
+@RequestMapping("/payments")
+public class PaymentController {
+
+    private final Map<Long, Payment> payments = new HashMap<>();
+
+    public PaymentController() {
+        payments.put(1L,new Payment(1L,100.0));
+        payments.put(2L,new Payment(2l,200.0));
+        payments.put(3L,new Payment(3l,300.0));
+
+    }
+
+    @GetMapping
+    public List<Payment> getPayments() {
+        return new ArrayList<>(payments.values());
+    }
+
+    @GetMapping("{id}")
+    public Payment getPayment(@PathVariable Long id) {
+        return payments.get(id);
+    }
+}
+
