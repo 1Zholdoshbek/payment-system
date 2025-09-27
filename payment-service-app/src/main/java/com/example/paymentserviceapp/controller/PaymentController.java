0061f43657e5e7d9176525577e46e7dd688736e9
1Zholdoshbek <zholdoshbekyashiev@gmail.com>
2025-09-27 16:59:22 +0600

    task-6

--- payment-service-app/src/main/java/com/example/paymentserviceapp/controller/PaymentController.java
+++ payment-service-app/src/main/java/com/example/paymentserviceapp/controller/PaymentController.java
@@ -1,36 +1,36 @@
 package com.example.paymentserviceapp.controller;
-import com.example.paymentserviceapp.model.Payment;
+import com.example.paymentserviceapp.persistence.entity.Payment;
+import com.example.paymentserviceapp.persistency.PaymentRepository;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
-import java.util.ArrayList;
-import java.util.HashMap;
 import java.util.List;
-import java.util.Map;
+import java.util.UUID;
 
 @RestController
 @RequestMapping("/payments")
 public class PaymentController {
 
-    private static final Map<Long, Payment> payments = new HashMap<>();
+    private final PaymentRepository paymentRepository;
 
-    static {
-        payments.put(1L, new Payment(1L, 100.0));
-        payments.put(2L, new Payment(2L, 200.0));
-        payments.put(3L, new Payment(3L, 300.0));
-
+    public PaymentController(PaymentRepository paymentRepository) {
+        this.paymentRepository = paymentRepository;
     }
+
 
     @GetMapping
     public List<Payment> getPayments() {
-        return new ArrayList<>(payments.values());
+        return paymentRepository.findAll();
     }
 
-    @GetMapping("{id}")
-    public Payment getPayment(@PathVariable Long id) {
-        return payments.get(id);
+    @GetMapping("{guid}")
+    public ResponseEntity<Payment> getPayment(@PathVariable UUID guid) {
+        return paymentRepository.findById(guid)
+                .map(ResponseEntity::ok)
+                .orElse(ResponseEntity.notFound().build());
     }
 }
 
